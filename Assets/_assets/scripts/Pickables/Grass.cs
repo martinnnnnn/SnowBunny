a842299ae75b70244f3f2d302de80ffa9c8bc5ca
martinnnnnn <martin.penard@gmail.com>
2019-12-02 20:59:22 +0100

    added grass sign

--- Assets/_assets/scripts/Pickables/Grass.cs
+++ Assets/_assets/scripts/Pickables/Grass.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
+
 
 public class Grass : MonoBehaviour
 {
@@ -14,15 +16,35 @@ public class Grass : MonoBehaviour
     Player player;
     InputAdapter inputAdapter;
 
+    [Header("Sign")]
+    public GameObject sign;
+
     private void Start()
     {
         player = FindObjectOfType<Player>();
         inputAdapter = FindObjectOfType<InputAdapter>();
+        sign.SetActive(false);
     }
 
     private void Update()
     {
         float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distanceToPlayer < pickupRadius)
+        {
+            sign.SetActive(true);
+            sign.transform.LookAt(Camera.main.transform);
+            if (inputAdapter.GetInputDown(InputAdapter.InputKey.A))
+            {
+                player.Eat(this);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            sign.SetActive(false);
+        }
+
         if (inputAdapter.GetInputDown(InputAdapter.InputKey.A) && distanceToPlayer < pickupRadius)
         {
             player.Eat(this);
